88ff7344dc6397e6d16d06a1583c46c92457d8e9
nexuiz-cc <hyd524132502@gmail.com>
2025-03-05 21:26:01 +0900

    init

---
+++ src/main/java/com/learn/nexuiz/NexuizApplication.java
@@ -0,0 +1,13 @@
+package com.learn.nexuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/nexuiz/NexuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.nexuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
